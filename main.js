946102f87b14a08de622c80130b7342c2f5eccf1
Joshua Johns <joshuajohns28@outlook.com>
2022-10-07 09:53:40 -0500

    first

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay", "hi" ];
+const toFireInKiln = [];
+
+for (const toco of clay) {
+   const mug = `${toco} coffee mug`
+   toFireInKiln.push(mug)
+};
+
+console.log(toFireInKiln);
